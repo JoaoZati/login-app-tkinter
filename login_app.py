41b60dec3c827be86e0678f60b3549ab875cd4e7
JoaoZati <joaozati@gmail.com>
2021-09-29 17:24:16 -0300

    Commit 1: - Mostrando como funciona o tkraise

--- login_app.py
+++ login_app.py
@@ -9,11 +9,21 @@ class LoginApp(tk.Tk):
         self.title('My App - Login')
         self.geometry('300x500')
         self.resizable(0, 0)
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
 
-        self.frame_start_page = StartPage(self, self)
-        self.frame_start_page.grid(column=0, row=0)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frame_start_page = StartPage(container, self)
+        self.frame_start_page.grid(row=0, column=0, sticky='nsew')
+
+        self.frame_2 = StartPage(container, self)
+        self.frame_2.config(bg='#212529')
+        self.frame_2.grid(row=0, column=0, sticky='nsew')
+
+        self.frame_2.tkraise()
+        self.frame_start_page.tkraise()
 
 
 if __name__ == '__main__':
